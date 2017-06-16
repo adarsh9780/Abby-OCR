fd1f1a4901152cd2f0ba9f539e7229bfb61870a9
adarsh9780 <adarshmaurya7@gmail.com>
2017-06-16 09:11:43 +0530

    Comments

--- TestAPI/views.py
+++ TestAPI/views.py
@@ -8,53 +8,58 @@ from AbbyyOnlineSdk import AbbyyOnlineSdk
 from time import sleep
 import os
 
+#Necessary for uploading the image from user to server
 class Upload(View):
 
+#if it is get request(user is coming first time to upload image )
     def get(self, request, *args, **kwargs):
+        #select the form to be displayed
         form = ImageForm()
-
+# and pass it to html page usng dictionary
         context = {
             'form' : form,
-            # 'CSS' : CSS,
         }
-        # print("Success")
         return render(request, 'TestAPI/upload.html', context)
 
+# after uploading image return to same page again
     def post(self, request, *args, **kwargs):
+        #some times usern won't be submitting anything to us via post request
+        # in that case don't capture any thing & just return to same page.
         form = ImageForm(request.POST or None, request.FILES or None)
+        # if user submits, verify the data, using inbuilt Django method,
         if form.is_valid():
             instance = form.save(commit = False)
             instance.save()
             return HttpResponseRedirect('/upload/')
-        
-        # CSS = os.path.join(settings.BASE_DIR, 
-        #     'TestAPI', 'templates', 'TestAPI', 'css', 'bootstrap.min.css')
-        
-        # JSS = os.path.join(settings.BASE_DIR, 
-        #     'TestAPI', 'templates', 'TestAPI', 'css', "bootstrap.min.js")
 
         context = {
         'form' : form,
-        # 'CSS' : CSS,
         }
         return render(request, 'TestAPI/upload.html', context)
 
-
+# settings required for ABBYY
 class Settings:
   Language = "English"
   OutputFormat = "txt"
 
+#when user clicks on "SHOW RESULT" send the uplloaded image to the server of abbyy
 class ViewText(View):
     
+    #using get method, capture the downloaded result
     def get(self, request, *args, **kwargs):
 
+# capture the latest image uploaded by user
         model = ImageModel.objects.all().order_by('-timestamp')[0]
+# location of image
         path = os.path.join(settings.MEDIA_ROOT, model.image.name )
+        #instance abbyy class
         a = AbbyyOnlineSdk()
         #returns an object with valuable info
         task = a.ProcessImage(path, Settings)
 
+#check for the status from abbyy server
         task_status = a.GetTaskStatus(task)
+# if it is not completed, repeat the preocess again in the interval of 2 secs.
         while(task_status.Status != 'Completed'):
           task_status = a.GetTaskStatus(task)
           sleep(2)
@@ -72,15 +77,8 @@ class ViewText(View):
         file_to_open = open(os.path.join(settings.BASE_DIR, output), 'r')
         text = file_to_open.read()
         
-        # CSS = os.path.join(settings.BASE_DIR, 
-        #     'TestAPI', 'templates', 'TestAPI', 'css', 'bootstrap.min.css')
-
-        # JSS = os.path.join(settings.BASE_DIR, 
-        #     'TestAPI', 'templates', 'TestAPI', 'css', "bootstrap.min.js")
-
         context = {
         'text':text,
-        # 'CSS':CSS,
         }
 
         file_to_open.close()
