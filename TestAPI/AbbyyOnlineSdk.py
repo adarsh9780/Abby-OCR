b3678008a1d86bfef5b8f04f1c0977cd9c8674a4
adarsh9780 <adarshmaurya7@gmail.com>
2017-06-17 09:53:48 +0530

    Username & Password Removed

--- TestAPI/AbbyyOnlineSdk.py
+++ TestAPI/AbbyyOnlineSdk.py
@@ -35,8 +35,8 @@ class AbbyyOnlineSdk:
 	# register at http://cloud.ocrsdk.com/Account/Register
 	# More info on getting your application id and password at
 	# http://ocrsdk.com/documentation/faq/#faq3
-	ApplicationId = ""
-	Password = ""
+	ApplicationId = "your user name"
+	Password = "your ocr-sdk password"
 	Proxy = None
 	enableDebugging = 0
 
